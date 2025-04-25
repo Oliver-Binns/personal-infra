f1823680d896af6bc67df1606d65e304ba6e3f1a
Oliver Binns <mail@oliverbinns.co.uk>
2025-04-25 08:29:39 +0100

    chore: fix file formatting

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   cloud {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "oliver-binns"
     workspaces {
       tags = ["personal-infra"]
@@ -19,20 +19,20 @@ terraform {
 
 provider "googleplay" {
   service_account_json_base64 = filebase64("~/service-account.json")
-  developer_id = "5166846112789481453"
+  developer_id                = "5166846112789481453"
 }
 
 resource "googleplay_user" "oliver" {
-  email = "mail@oliverbinns.info"
+  email              = "mail@oliverbinns.info"
   global_permissions = ["CAN_MANAGE_PERMISSIONS_GLOBAL"]
 }
 
 resource "googleplay_user" "service_account" {
-  email = "google-play-console@seraphic-elixir-305011.iam.gserviceaccount.com"
+  email              = "google-play-console@seraphic-elixir-305011.iam.gserviceaccount.com"
   global_permissions = ["CAN_MANAGE_PERMISSIONS_GLOBAL"]
 }
 
 resource "googleplay_user" "example" {
-  email = "example@oliverbinns.info"
+  email              = "example@oliverbinns.info"
   global_permissions = ["CAN_VIEW_APP_QUALITY_GLOBAL"]
 }
