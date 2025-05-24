bd058ea5ada18ba774f268370562a5350182cce7
Oliver Binns <mail@oliverbinns.co.uk>
2025-05-24 14:43:06 +0100

    feat: manage GitHub repository

---
+++ github.tf
@@ -0,0 +1,27 @@
+provider "github" { }
+
+resource "github_repository" "ios" {
+  name        = "Crescendo"
+  description = "This app allows you to play Apple Music playlists for timed musical party games, such as pass the parcel or musical chairs."
+
+  visibility = "public"
+
+  allow_merge_commit = false
+  allow_rebase_merge = false
+
+  archive_on_destroy = true
+
+  license_template = "mit"
+}
+
+resource "github_branch_protection" "ios" {
+  repository_id = github_repository.ios.node_id
+  pattern          = "main"
+ 
+  require_signed_commits = true
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    require_code_owner_reviews = true
+  }
+}
