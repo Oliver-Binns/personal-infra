e2a3b5bdbc133ae73b46e4091e058a079e90ac73
Oliver Binns <mail@oliverbinns.co.uk>
2025-11-16 16:34:25 +0000

    feat: enable cloudbilling api

--- bootstrap/services.tf
+++ bootstrap/services.tf
@@ -1,6 +1,7 @@
 resource "google_project_service" "service" {
   for_each = toset([
     "apikeys.googleapis.com",
+    "cloudbilling.googleapis.com",
     "cloudresourcemanager.googleapis.com",
     "iam.googleapis.com",
     "iamcredentials.googleapis.com"
