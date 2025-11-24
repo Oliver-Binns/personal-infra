6ebeb703698616ee7c483a2a9ec1c24a6205b922
Oliver Binns <mail@oliverbinns.co.uk>
2025-11-24 17:48:10 +0000

    feat: bootstrap Apply workload federation (#18)

--- bootstrap/workload_identity.tf
+++ bootstrap/workload_identity.tf
@@ -90,6 +90,66 @@ resource "google_project_iam_member" "plan_role" {
   role    = google_project_iam_custom_role.plan.name
 }
 
+# Permissions - Apply
+
+resource "google_service_account_iam_member" "apply" {
+  service_account_id = google_service_account.apply.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principal://iam.googleapis.com/${google_iam_workload_identity_pool.apply.name}/subject/repo:Oliver-Binns/personal-infra:ref:refs/heads/main"
+}
+
+resource "google_storage_bucket_iam_member" "service_account_tf_state_apply" {
+  bucket = google_storage_bucket.state.name
+
+  # write permissions are required for state lock
+  role   = "roles/storage.objectUser"
+  member = "serviceAccount:${google_service_account.apply.email}"
+}
+
+resource "google_organization_iam_member" "apply" {
+  for_each = toset([
+    "roles/viewer",
+    "roles/billing.admin",
+    "roles/iam.organizationRoleAdmin",
+  ])
+
+  org_id = data.google_organization.default.org_id
+  member = "serviceAccount:${google_service_account.apply.email}"
+  role   = each.value
+}
+
+resource "google_project_iam_member" "apply" {
+  for_each = toset([
+    "roles/iam.roleAdmin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.workloadIdentityPoolAdmin",
+    "roles/resourcemanager.projectIamAdmin",
+  ])
+
+  project = google_project.default.project_id
+  member  = "serviceAccount:${google_service_account.apply.email}"
+  role    = each.value
+}
+
+resource "google_project_iam_custom_role" "apply" {
+  role_id     = "TFApply"
+  title       = "TF Apply"
+  description = "Custom role to support additional actions when applying"
+  permissions = toset([
+    "serviceusage.services.get",
+    "serviceusage.services.list",
+    "storage.buckets.get",
+    "storage.buckets.getIamPolicy",
+    "storage.objects.getIamPolicy",
+  ])
+}
+
+resource "google_project_iam_member" "apply_role" {
+  project = google_project.default.project_id
+  member  = "serviceAccount:${google_service_account.apply.email}"
+  role    = google_project_iam_custom_role.apply.name
+}
+
 # Identity Providers - GitHub
 
 resource "google_iam_workload_identity_pool_provider" "plan" {
@@ -106,6 +166,29 @@ resource "google_iam_workload_identity_pool_provider" "plan" {
 
   attribute_condition = <<-EOT
       attribute.org == "Oliver-Binns"
+      && attribute.repository == "Oliver-Binns/personal-infra"
+    EOT
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_iam_workload_identity_pool_provider" "apply" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.apply.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-actions"
+  display_name                       = "GitHub Actions"
+  description                        = "OIDC identity pool provider for Terraform Applys run in GitHub Actions"
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+    "attribute.org"        = "assertion.repository_owner"
+  }
+
+  attribute_condition = <<-EOT
+      google.subject == 'repo:Oliver-Binns/personal-infra:ref:refs/heads/main'
+      && attribute.org == "Oliver-Binns"
       && attribute.repository == "Oliver-Binns/personal-infra"
     EOT
 
