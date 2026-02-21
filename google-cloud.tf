61ddf3c414a562686f93619bf92eeb86db9639bf
Oliver Binns <mail@oliverbinns.co.uk>
2026-02-21 13:56:01 +0000

    Setup Google Cloud project for wedding (#22)

--- google-cloud.tf
+++ google-cloud.tf
@@ -1,3 +1,12 @@
 provider "google" {
   project = local.google_project_id
 }
+
+data "google_organization" "default" {
+  domain = "oliverbinns.info"
+}
+
+data "google_billing_account" "default" {
+  billing_account = "billingAccounts/016282-310295-698AE7"
+  open            = true
+}
