e32198e80f09c5051724447dc45b7ffac254b9fb
Oliver Binns <mail@oliverbinns.co.uk>
2026-02-21 15:23:26 +0000

    Use globally unique for GCP wedding project (#26)

--- wedding.tf
+++ wedding.tf
@@ -1,6 +1,6 @@
 resource "google_project" "wedding" {
   name       = "Happily Ever After"
-  project_id = "happily-ever-after"
+  project_id = "obinns-happily-ever-after"
 
   org_id          = data.google_organization.default.org_id
   billing_account = data.google_billing_account.default.id
