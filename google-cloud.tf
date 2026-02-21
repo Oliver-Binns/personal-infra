7d5314bff207f7281d1243d6651a43ec901f63f0
Oliver Binns <mail@oliverbinns.co.uk>
2026-02-21 13:46:31 +0000

    Fetch Google Billing Account details

--- google-cloud.tf
+++ google-cloud.tf
@@ -5,3 +5,8 @@ provider "google" {
 data "google_organization" "default" {
   domain = "oliverbinns.info"
 }
+
+data "google_billing_account" "default" {
+  billing_account = "billingAccounts/016282-310295-698AE7"
+  open            = true
+}
