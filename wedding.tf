04cfc5b64d204242d5b1ecd5ecf8ccf0a35f439b
Oliver Binns <mail@oliverbinns.co.uk>
2026-02-21 13:50:33 +0000

    Fix format of wedding.tf

--- wedding.tf
+++ wedding.tf
@@ -1,7 +1,7 @@
 resource "google_project" "wedding" {
-  name       = "Happily Ever After"
-  project_id = "happily-ever-after"
+  name            = "Happily Ever After"
+  project_id      = "happily-ever-after"
 
-  org_id     = data.google_organization.default.org_id
+  org_id          = data.google_organization.default.org_id
   billing_account = data.google_billing_account.default.i
 }
