15c19de493a69863de7af28fbeed3d4ade81c0c6
Oliver Binns <mail@oliverbinns.co.uk>
2026-02-21 14:14:11 +0000

    Create new Firebase project for Wedding

--- wedding.tf
+++ wedding.tf
@@ -5,3 +5,8 @@ resource "google_project" "wedding" {
   org_id          = data.google_organization.default.org_id
   billing_account = data.google_billing_account.default.id
 }
+
+resource "google_firebase_project" "default" {
+  provider = google-beta
+  project  = google_project.wedding.project_id
+}
