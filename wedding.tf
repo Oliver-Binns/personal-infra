956e81553908d1617894a0607419ed53902d262e
Oliver Binns <mail@oliverbinns.co.uk>
2026-02-21 13:51:35 +0000

    Fix syntax error in wedding.tf

--- wedding.tf
+++ wedding.tf
@@ -3,5 +3,5 @@ resource "google_project" "wedding" {
   project_id      = "happily-ever-after"
 
   org_id          = data.google_organization.default.org_id
-  billing_account = data.google_billing_account.default.i
+  billing_account = data.google_billing_account.default.id
 }
